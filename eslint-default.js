670102017acad7f8d1112000007352199d390840
Augustin Šulc <sulc.gusta@gmail.com>
2020-12-10 16:34:04 +0100

    Eslint updated, tslint removed

--- eslint-default.js
+++ eslint-default.js
@@ -1,32 +1,43 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
   parserOptions: {
     project: ["tsconfig.json"],
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
+      jsx: true, // Allows for the parsing of JSX
+    },
   },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/recommended",
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
   rules: {
+    // disable the 'no-use-before-define' rule as it can report incorrect errors https://stackoverflow.com/a/64024916/19712
+    "@typescript-eslint/no-use-before-define": "off",
+
+    "no-redeclare": "off", // watns on typescript function overrides
+    //"@typescript-eslint/no-redeclare": ["error"], // this should be enabled, but causes 'Definition for rule '@typescript-eslint/no-redeclare' was not found'
+
     "react/prop-types": "off",
     "react/display-name": "off",
+
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": [1, { args: "none", ignoreRestSiblings: true }],
+    "@typescript-eslint/no-unused-vars": [
+      1,
+      { args: "none", ignoreRestSiblings: true },
+    ],
     "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/tslint/config": [1, { lintFile: "./tslint.json" }]
   },
-  plugins: ["@typescript-eslint/tslint"], // tslint integration is used for tslint-sonarts. Other rules should be handled by ESlint.
   settings: {
     react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
-  }
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
 };
