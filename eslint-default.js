3d526542a8e5f478da51349f1ffde3f5dee57ded
Augustin Šulc <sulc.gusta@gmail.com>
2021-03-17 09:17:04 +0100

    New rules enabled: curly, react/jsx-curly-brace-presence

--- eslint-default.js
+++ eslint-default.js
@@ -19,22 +19,22 @@ module.exports = {
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
-    // disable the 'no-use-before-define' rule as it can report incorrect errors https://stackoverflow.com/a/64024916/19712
-    "@typescript-eslint/no-use-before-define": "off",
-
-    "no-redeclare": "off", // watns on typescript function overrides
+    curly: ["error", "all"],
+    "no-redeclare": "off", // warns on typescript function overrides
     //"@typescript-eslint/no-redeclare": ["error"], // this should be enabled, but causes 'Definition for rule '@typescript-eslint/no-redeclare' was not found'
 
     "react/prop-types": "off",
     "react/display-name": "off",
+    "react/jsx-curly-brace-presence": ["error", "never"],
 
     "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": [
       1,
       { args: "none", ignoreRestSiblings: true },
     ],
-    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-use-before-define": "off", // disabled because it can report incorrect errors https://stackoverflow.com/a/64024916/19712
   },
   settings: {
     react: {
