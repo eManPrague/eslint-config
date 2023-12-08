5d63ea438ab11794ccd320a4adb8c670ca7b06a8
Dominik Bláha <dominikblaha@Dominikuv-MacBook-Pro.local>
2023-12-08 17:41:53 +0100

    Version 3.4.0

---
+++ eslint-react.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  plugins: ["react"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:storybook/recommended",
+    "./eslint-default.js",
+  ],
+  rules: {
+    curly: ["error", "all"],
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "react/jsx-curly-brace-presence": ["error", "never"],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-key": ["warn"],
+  },
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
